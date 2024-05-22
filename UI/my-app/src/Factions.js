6e9a3ad09bd2fa779e68069786c352bc285fea56
catamateas <mateas.catalin16@gmail.com>
2024-05-22 16:19:43 +0300

    Factions Page Redy 48:03

--- UI/my-app/src/Factions.js
+++ UI/my-app/src/Factions.js
@@ -1,35 +1,129 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
 import { variables } from "./Variables";
 
-export class Factions extends Component{
+export class Factions extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
 
-        this.state={
-            factions:[]
+        this.state = {
+            factions: [],
+            modalTitle: "",
+            FactionsName: "",
+            FactionsId: 0
         }
     }
 
-    refreshList(){
-        fetch(variables.API_URL+'factions')
-        .then(response=>response.json())
-        .then(data=>{
-            this.setState({factions:data});
-        });
+    refreshList() {
+        fetch(variables.API_URL + 'factions')
+            .then(response => response.json())
+            .then(data => {
+                this.setState({ factions: data });
+            });
     }
 
-    componentDidMount(){
+    componentDidMount() {
         this.refreshList();
     }
 
-    render(){
-        const {
-            factions
-        }=this.state;
+    changeFactionsName = (e) => {
+        this.setState({ FactionsName: e.target.value });
+    }
 
-        return(
+    addClick() {
+        this.setState({
+            modalTitle: "Add Factions",
+            FactionsId: 0,
+            FactionsName: ""
+        });
+    }
+
+    editClick(fac) {
+        this.setState({
+            modalTitle: "Edit Factions",
+            FactionsId: fac.FactionsId,
+            FactionsName: fac.FactionsName
+        });
+    }
+
+    createClick(){
+        fetch(variables.API_URL_+'factions',{
+            method:'POST',
+            headers:{
+                'Accept':'application/json',
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify({
+                FactionsName:this.state.FactionsName
+            })
+        })
+        .then(res=>res.json())
+        .then((result)=>{
+            alert(result);
+            this.refreshList();
+        },(error)=>{
+            alert('Failed');
+        })
+    }
+
+    updateClick(){
+        fetch(variables.API_URL_+'factions',{
+            method:'PUT',
+            headers:{
+                'Accept':'application/json',
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify({
+                FactionsId:this.state.FactionsId,
+                FactionsName:this.state.FactionsName
+            })
+        })
+        .then(res=>res.json())
+        .then((result)=>{
+            alert(result);
+            this.refreshList();
+        },(error)=>{
+            alert('Failed');
+        })
+    }
+
+    deleteClick(id){
+        if(window.confirm('Are you sure?')){
+        fetch(variables.API_URL_+'factions/'+id,{
+            method:'DELETE',
+            headers:{
+                'Accept':'application/json',
+                'Content-Type':'application/json'
+            }
+        })
+        .then(res=>res.json())
+        .then((result)=>{
+            alert(result);
+            this.refreshList();
+        },(error)=>{
+            alert('Failed');
+        })
+        }
+    }
+
+
+    render() {
+        const {
+            factions,
+            modalTitle,
+            FactionsId,
+            FactionsName
+        } = this.state;
+
+        return (
             <div>
+                <button type="button"
+                    className="btn btn-primary m-2 float-end"
+                    data-bs-toggle="modal"
+                    data-bs-target="#exampleModal"
+                    onClick={() => this.addClick()}>
+                    Add Factions
+                </button>
                 <table className="table table-striped">
                     <thead>
                         <tr>
@@ -45,34 +139,69 @@ export class Factions extends Component{
                         </tr>
                     </thead>
                     <tbody>
-                        {factions.map(fac=>
+                        {factions.map(fac =>
                             <tr key={fac.FactionsId}>
                                 <td>{fac.FactionsId}</td>
                                 <td>{fac.FactionsName}</td>
                                 <td>
                                     <button type="button"
-                                    className="btn btn-light mr-1">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-                                        <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-                                        <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
+                                        className="btn btn-light mr-1"
+                                        data-bs-toggle="modal"
+                                        data-bs-target="#exampleModal"
+                                        onClick={() => this.editClick(fac)}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16">
+                                            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                            <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
                                         </svg>
                                     </button>
 
                                     <button type="button"
-                                    className="btn btn-light mr-1">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
-                                        <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5"/>
+                                        className="btn btn-light mr-1"
+                                        onClick={()=>this.deleteClick(fac.FactionsId)}>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash3-fill" viewBox="0 0 16 16">
+                                            <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5m-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5M4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06m6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528M8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5" />
                                         </svg>
                                     </button>
-
-
                                 </td>
-
-
                             </tr>
                         )}
                     </tbody>
                 </table>
+
+                <div className="modal fade" id="exampleModal" tabIndex="-1" aria-hidden="true">
+                    <div className="modal-dialog modal-lg modal-dialog-centered">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title">{modalTitle}</h5>
+                                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div className="modal-body">
+                                <div className="input-group mb-3">
+                                    <span className="input-group-text">FactionsName</span>
+                                    <input type="text" className="form-control"
+                                        value={FactionsName}
+                                        onChange={this.changeFactionsName} />
+                                </div>
+
+                                {FactionsId == 0 ?
+                                    <button type="button"
+                                        className="btn btn-primary float-start"
+                                        onClick={()=>this.createClick()}
+                                    >Create</button>
+                                    : null}
+
+                                {FactionsId != 0 ?
+                                    <button type="button"
+                                        className="btn btn-primary float-start"
+                                        onClick={()=>this.updateClick()}
+                                    >Update</button>
+                                    : null}
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
         )
     }
