6e82fed780e3e086ea159739aaf2d489093b1824
AlDobrovolski <dobrovolschi.ol.al@student.utcluj.ro>
2024-05-23 00:42:08 +0300

    functionalitati complaint si tickets

--- API/WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ API/WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b98e550cb0acf39d2e362d03298fc373d8439e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a397f6fb96c71a4657263e7460ffcce11e85bb3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
