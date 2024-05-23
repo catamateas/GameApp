9f63ce77bd01ce0c961f3899baf86fdba77b8856
catamateas <mateas.catalin16@gmail.com>
2024-05-23 21:15:13 +0300

    merged Front v3

--- API/WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ API/WebApplication1/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ebb5906ff32880904922be5cf5df4f56be775d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96a428454f87ea7028a3479a77e90b393c8a1872")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
